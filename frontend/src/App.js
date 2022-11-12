b7e6b10ff52d4402f6279084a3310073999511a5
aryan <aaryan.p114@gmail.com>
2022-11-12 14:10:43 +0530

    gg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,37 @@
+import { useEffect } from "react";
+import UnlSdk from "unl-map-js";
 function App() {
+  // function loadJS(FILE_URL) {
+  //   let scriptEle = document.createElement("script");
+  //   scriptEle.setAttribute("src", FILE_URL);
+  //   scriptEle.setAttribute("type", "text/javascript");
+  //   scriptEle.setAttribute("async", true);
+  //   document.body.appendChild(scriptEle);
+  //   scriptEle.addEventListener("load", () => {
+  //     var map = new UnlSdk.Map({
+  //       apiKey: "Hf4oGgFB2E23aiLLUgDqkAGrrpAA9JxL",
+  //       vpmId: "eda1b140-0ea8-43ac-849b-eadb9d99f608",
+  //       gridControl: true,
+  //       indoorMapsControl: true,
+  //       tilesSelectorControl: true,
+  //       draftShapesControl: true,
+  //       container: "map",
+  //       center: [72.8777, 19.076],
+  //       zoom: 10,
+  //       minZoom: 2,
+  //     });
+  //   });
+  //   scriptEle.addEventListener("error", (ev) => {
+  //     console.log("Error on loading file", ev);
+  //   });
+  // }
+  // loadJS("https://unpkg.com/unl-map-js@0.0.3/lib/unl-map-js.js");
+  useEffect(() => {
+    // require("https://unpkg.com/unl-map-js@0.0.3/lib/unl-map-js.js")
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="map" style={{ width: "100vh", height: "100vh" }}></div>
     </div>
   );
 }
